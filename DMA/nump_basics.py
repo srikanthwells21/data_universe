3a1c96f5412435f77cc2a8182b4def360c3e9505
srikanthwells21 <srikanthwells21@gmail.com>
2026-06-04 09:38:14 +0530

    numpy operations

--- DMA/nump_basics.py
+++ DMA/nump_basics.py
@@ -169,3 +169,111 @@ print(min_c)
     
 Sum = (max_c +min_c)
 print(Sum)
+
+#Numpy Array Operations
+u = np.array([1, 0])
+print(u)
+
+v = np.array([0, 1])
+print(v)
+
+# Numpy Array Addition
+
+z = np.add(u, v)
+print(z)
+
+# Plotting functions
+
+
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+
+def Plotvec1(u, z, v):
+    
+    ax = plt.axes() # to generate the full window axes
+    ax.arrow(0, 0, *u, head_width=0.05, color='r', head_length=0.1)# Add an arrow to the  U Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(u + 0.1), 'u')#Adds the text u to the Axes 
+    
+    ax.arrow(0, 0, *v, head_width=0.05, color='b', head_length=0.1)# Add an arrow to the  v Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(v + 0.1), 'v')#Adds the text v to the Axes 
+    
+    ax.arrow(0, 0, *z, head_width=0.05, head_length=0.1)
+    plt.text(*(z + 0.1), 'z')#Adds the text z to the Axes 
+    plt.ylim(-2, 2)#set the ylim to bottom(-2), top(2)
+    plt.xlim(-2, 2)#set the xlim to left(-2), right(2)
+
+# Plot numpy arrays
+
+Plotvec1(u, z, v)
+
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+# Enter your code here
+arr3 = np.add(arr1, arr2)
+print(arr3)
+
+a = np.array([10, 20, 30])
+a
+
+#Array Subtraction¶
+a = np.array([10, 20, 30])
+a
+
+b = np.array([5, 10, 15])
+b
+
+c = np.subtract(a, b)
+
+print(c)
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+# Enter your code here
+arr3 = np.subtract(arr1, arr2)
+print(arr3)
+
+#Array Multiplication¶
+# Create a numpy array
+
+x = np.array([1, 2])
+x
+
+# Create a numpy array
+
+y = np.array([2, 1])
+y
+
+# Numpy Array Multiplication
+
+z = np.multiply(x, y)
+print(z)
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([2, 1, 2, 3, 4, 5])
+
+# Enter your code here
+arr3 = np.multiply(arr1, arr2)
+print(arr3)
+
+#Array Division¶
+a = np.array([10, 20, 30])
+a
+
+b = np.array([2, 10, 5])
+b
+
+c = np.divide(a, b)
+print(c)
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 5, 10, 8, 2, 33])
+
+# Enter your code here
+arr3 = np.divide(arr1, arr2)
+print(arr3)
